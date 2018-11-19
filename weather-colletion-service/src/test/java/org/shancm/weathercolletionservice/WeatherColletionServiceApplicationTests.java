1fdea28c70f5774c3582da4a5a84a977e0f7c2ec
Shancm <ruan-shan@163.com>
2018-11-19 18:57:49 +0800

    shancm 11.19

---
+++ weather-colletion-service/src/test/java/org/shancm/weathercolletionservice/WeatherColletionServiceApplicationTests.java
@@ -0,0 +1,16 @@
+package org.shancm.weathercolletionservice;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class WeatherColletionServiceApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
