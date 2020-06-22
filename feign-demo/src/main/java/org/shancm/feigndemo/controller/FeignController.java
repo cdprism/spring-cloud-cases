07ce84c55329ff360cb15cec9a742505dd92b807
cdprism <ruan-shan@163.com>
2020-06-22 16:04:33 +0800

    none

--- feign-demo/src/main/java/org/shancm/feigndemo/controller/FeignController.java
+++ feign-demo/src/main/java/org/shancm/feigndemo/controller/FeignController.java
@@ -3,6 +3,7 @@ package org.shancm.feigndemo.controller;
 import org.shancm.feigndemo.service.HelloService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
  * @date 2019/1/18
  */
 @RestController
+@RequestMapping("/feign")
 public class FeignController {
 
 	private final HelloService helloService;
