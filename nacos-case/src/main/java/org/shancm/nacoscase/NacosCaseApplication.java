f7cd38acd83b1ec1ed0393859400ea8a5908e7ee
cdprism <ruan-shan@163.com>
2020-06-24 18:38:48 +0800

    create module nacos

---
+++ nacos-case/src/main/java/org/shancm/nacoscase/NacosCaseApplication.java
@@ -0,0 +1,13 @@
+package org.shancm.nacoscase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NacosCaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NacosCaseApplication.class, args);
+    }
+
+}
---
+++ nacos-case/src/test/java/org/shancm/nacoscase/NacosCaseApplicationTests.java
@@ -0,0 +1,13 @@
+package org.shancm.nacoscase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NacosCaseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
